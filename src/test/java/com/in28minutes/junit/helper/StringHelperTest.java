1c80029e57eba3a330db346ab1c4192d6d4054f5
VitorLima <vitorgl@ciandt.com>
2022-07-17 19:01:39 -0300

    Refactoring Your First JUnit Test 2

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -9,17 +9,33 @@ import static org.junit.Assert.assertEquals;
 
 public class StringHelperTest {
 
+    StringHelper helper = new StringHelper();
+
     @Test
-    public void test() {
+    public void testTruncateAInFirst2Positions_AinFirst2Positions() {
 
         StringHelper helper = new StringHelper();
 
-
         // AACD => CC  ACD => CD CDEF => CDEF CDAA => CDAA
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
         // expected, actual
     }
 
+    @Test
+    public void testTruncateAInFirst2Positions_AinFirst2Positions2(){
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions2_AinFirst2Positions3(){
+        // AACD => CC  ACD => CD CDEF => CDEF CDAA => CDAA
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEFJ"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions2_AinFirst2Positions4() {
+        // AACD => CC  ACD => CD CDEF => CDEF CDAA => CDAA
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+    }
 
 }
