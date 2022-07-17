b005a6aedf7f3809f66dcbc51f425ca3b3bf5761
VitorLima <vitorgl@ciandt.com>
2022-07-17 20:08:59 -0300

    Testing Exceptions in JUnit Tests

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -11,12 +11,17 @@ public class ArraysCompareTest {
 
     //Arrays.sort
     @Test
-    public void testArraySorte_RandomArray(){
-        int[] numbers = {12,3,4,1};
-        int[] expected = {1,3,4,12};
+    public void testArraySorte_RandomArray() {
+        int[] numbers = {12, 3, 4, 1};
+        int[] expected = {1, 3, 4, 12};
         Arrays.sort(numbers);
         assertArrayEquals(expected, numbers);
-
-
     }
-}
+
+    @Test(expected=NullPointerException.class)
+    public void testArraySort_NullArray() {
+        int[] numbers = null;
+            Arrays.sort(numbers);
+
+        }
+    }
