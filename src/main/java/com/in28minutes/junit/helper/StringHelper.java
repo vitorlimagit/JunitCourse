e3652adabf727afbd85216996bb3c8d8e82933ef
VitorLima <vitorgl@ciandt.com>
2022-07-17 19:27:16 -0300

    Second Junit Example assertTrue and False

--- src/main/java/com/in28minutes/junit/helper/StringHelper.java
+++ src/main/java/com/in28minutes/junit/helper/StringHelper.java
@@ -14,6 +14,7 @@ public class StringHelper {
 				+ stringMinusFirst2Chars;
 	}
 
+	// ABCD => flase, ABAB => true, AB => true, A => false,
 	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
 
 		if (str.length() <= 1)
--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -4,7 +4,7 @@ package com.in28minutes.junit.helper;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class StringHelperTest {
@@ -29,13 +29,42 @@ public class StringHelperTest {
     @Test
     public void testTruncateAInFirst2Positions2_AinFirst2Positions3(){
         // AACD => CC  ACD => CD CDEF => CDEF CDAA => CDAA
-        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEFJ"));
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
     }
 
     @Test
     public void testTruncateAInFirst2Positions2_AinFirst2Positions4() {
         // AACD => CC  ACD => CD CDEF => CDEF CDAA => CDAA
         assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+    }
+
+    // ABCD => flase, ABAB => true, AB => true, A => false,//
+
+   @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario(){
+       assertFalse(
+               helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+   }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario(){
+        // ABCD => flase, ABAB => true, AB => true, A => false,//
+        assertTrue(
+                helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_AdvancedPositiveScenario(){
+        // ABCD => flase, ABAB => true, AB => true, A => false,//
+        assertTrue(
+                helper.areFirstAndLastTwoCharactersTheSame("AB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_AdvancedNegativeScenario(){
+        // ABCD => flase, ABAB => true, AB => true, A => false,//
+        assertFalse(
+                helper.areFirstAndLastTwoCharactersTheSame("A"));
     }
 
 }
