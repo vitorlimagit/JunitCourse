027c9119e627b8367a072b471d9de4881c760713
VitorLima <vitorgl@ciandt.com>
2022-07-17 20:31:43 -0300

    Testing Performance in JUnit Tests

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -24,4 +24,15 @@ public class ArraysCompareTest {
             Arrays.sort(numbers);
 
         }
+
+        @Test(timeout = 100)
+    public void testSort_Performance(){
+
+        int array[] = {12,23,4};
+            for(int i=1; i <=1000000; i++){
+                array[0] = i;
+                Arrays.sort(array);
+            }
+
+        }
     }
