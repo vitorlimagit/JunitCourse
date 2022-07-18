a01d82862f43deb6231bd3984b4e00ce8f482a25
VitorLima <vitorgl@ciandt.com>
2022-07-17 21:08:55 -0300

    Organize JUnits into Suites Tests

---
+++ src/test/java/com/in28minutes/junit/helper/AllTests.java
@@ -0,0 +1,10 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({QuickBeforeAfterTest.class, StringHelperTest.class})
+public class AllTests {
+
+}
